8bd2062c16d42aa3d9f5b15e7c4e4588d7320819
legend19410 <francismilton19410@gmail.com>
2020-11-15 10:54:46 -0500

    Exercise 4

--- world.js
+++ world.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
     //get the look up button
     const lookupBtn = document.getElementById('lookup');
+    const lookupCitiesBtn = document.getElementById('lookup-cities');
 
     lookupBtn.addEventListener('click',function(event){
-        event.preventDefault();
         const country = document.getElementById('country');
         const result = document.getElementById('result');
         let requested = country.name+"="+country.value;
@@ -22,4 +22,26 @@ document.addEventListener('DOMContentLoaded', function(){
        // xhr.setRequestHeader("content-type", 'application/x-www-form-urlencoded');
         xhr.send();
     });
+
+    lookupCitiesBtn.addEventListener('click',function(event){
+        const country = document.getElementById('country');
+        const result = document.getElementById('result');
+        let requested = "country="+country.value+"&context=cities";
+        console.log(requested);
+
+        const xhr = new XMLHttpRequest();
+
+        // when we receive the contents from the server
+        xhr.onload = function(){
+            console.log(this.responseText);
+            result.innerHTML = this.responseText;
+
+        }
+
+        xhr.open("GET", "world.php?"+requested, true);
+       // xhr.setRequestHeader("content-type", 'application/x-www-form-urlencoded');
+        xhr.send();
+    });
+
+
 });
--- world.php
+++ world.php
@@ -5,34 +5,77 @@ $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$country = filter_var(trim($_GET["country"]), FILTER_SANITIZE_STRING);
-if($country){
-  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-}else{
-  $stmt = $conn->query("SELECT * FROM countries");
+
+if(isset($_GET["context"])){
+  $country = filter_var(trim($_GET["country"]), FILTER_SANITIZE_STRING);
+  if($country){
+    //$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $stmt = $conn->query("SELECT * FROM countries JOIN cities ON cities.country_code=countries.code WHERE countries.name LIKE '%$country%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo '<table>';
+      echo '<thead>';
+        echo '<tr>';
+          echo "<th>Name</th>";
+          echo "<th>District</th>";
+          echo "<th>Population</th>";
+        echo '</tr>';
+      echo '</thead>';
+      echo '<tbody>';
+      foreach($results as $row){
+        echo "<tr>";
+          echo "<td>".$row['name']."</td>";
+          echo "<td>".$row['district']."</td>";
+          echo "<td>".$row['population']."</td>";
+        echo "</tr>";
+      }
+      echo '</tbody>';
+    echo '</table>';
+  }else{
+    $stmt = $conn->query("SELECT * FROM countries");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    display_country_table($results);
+  }
+
+  
+
+}elseif(isset($_GET["country"])){
+  $country = filter_var(trim($_GET["country"]), FILTER_SANITIZE_STRING);
+  if($country){
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  }else{
+    $stmt = $conn->query("SELECT * FROM countries");
+  }
+
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  display_country_table($results);
+
 }
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo '<table>';
-  echo '<thead>';
-    echo '<tr>';
-      echo "<th>Name</th>";
-      echo "<th>Continent</th>";
-      echo "<th>Independence</th>";
-      echo "<th>Head of State</th>";
-    echo '</tr>';
-  echo '</thead>';
-  echo '<tbody>';
-  foreach($results as $row){
-    echo "<tr>";
-      echo "<td>".$row['name']."</td>";
-      echo "<td>".$row['continent']."</td>";
-      echo "<td>".$row['independence_year']."</td>";
-      echo "<td>".$row['head_of_state']."</td>";
-    echo "</tr>";
-  }
-  echo '</tbody>';
-echo '</table>';
+function display_country_table($results){
+  echo '<table>';
+    echo '<thead>';
+      echo '<tr>';
+        echo "<th>Name</th>";
+        echo "<th>Continent</th>";
+        echo "<th>Independence</th>";
+        echo "<th>Head of State</th>";
+      echo '</tr>';
+    echo '</thead>';
+    echo '<tbody>';
+    foreach($results as $row){
+      echo "<tr>";
+        echo "<td>".$row['name']."</td>";
+        echo "<td>".$row['continent']."</td>";
+        echo "<td>".$row['independence_year']."</td>";
+        echo "<td>".$row['head_of_state']."</td>";
+      echo "</tr>";
+    }
+    echo '</tbody>';
+  echo '</table>';
+}
+
+
+
 
 
 
