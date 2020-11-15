a4696bc8a1558a0d5596da7e583fbf54ab90176d
legend19410 <francismilton19410@gmail.com>
2020-11-15 11:00:35 -0500

    Exercise 5

--- world.js
+++ world.js
@@ -19,7 +19,6 @@ document.addEventListener('DOMContentLoaded', function(){
         }
 
         xhr.open("GET", "world.php?"+requested, true);
-       // xhr.setRequestHeader("content-type", 'application/x-www-form-urlencoded');
         xhr.send();
     });
 
